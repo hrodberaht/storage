d6ff4c2c84d57f9d9e6b90294a364e7f2991aa55
Robert <seedraev@gmail.com>
2018-08-30 14:13:07 +0200

    add price-chenge model and mock

---
+++ src/app/main/shared/mock-price-change.ts
@@ -0,0 +1,9 @@
+import { PriceChange } from "./price-change.model";
+
+const PRICECHANGE: PriceChange = [
+  {
+    auctionId: 234234432,
+    hasChange: true,
+    price: 12.5
+  }
+];
---
+++ src/app/main/shared/price-change.model.ts
@@ -0,0 +1,5 @@
+export class PriceChange {
+  auctionId: number;
+  hasChange: boolean;
+  newPrice: number;
+}
