de954cf537ae114ec7a007f69890cd4f69045b2a
hrodberaht <robpszczolka@gmail.com>
2018-09-10 10:21:53 +0200

    add quantiti to products and show it i product list

--- src/app/main/shared/mock-product.ts
+++ src/app/main/shared/mock-product.ts
@@ -1,68 +1,76 @@
-import { Product } from "./product.model";
+import { Product } from './product.model';
 
 export const PRODUCT: Product[] = [
   {
     id: 1,
-    name: "XBOX",
+    name: 'XBOX',
     ean: 5326523535,
     price: 12.5,
     imgUrl:
-      "https://compass-ssl.xbox.com/assets/a9/d0/a9d03015-2760-43df-8056-d23af1969289.gif?n=xbox_facebook_200x200.gif"
+      'https://compass-ssl.xbox.com/assets/a9/d0/a9d03015-2760-43df-8056-d23af1969289.gif?n=xbox_facebook_200x200.gif',
+    quantity: 0
   },
   {
     id: 2,
-    name: "XBOX",
+    name: 'XBOX',
     ean: 324576475674,
     price: 12.5,
     imgUrl:
-      "https://compass-ssl.xbox.com/assets/a9/d0/a9d03015-2760-43df-8056-d23af1969289.gif?n=xbox_facebook_200x200.gif"
+      'https://compass-ssl.xbox.com/assets/a9/d0/a9d03015-2760-43df-8056-d23af1969289.gif?n=xbox_facebook_200x200.gif',
+    quantity: 0
   },
   {
     id: 3,
-    name: "XBOX",
+    name: 'XBOX',
     ean: 67585868678,
     price: 12.5,
     imgUrl:
-      "https://compass-ssl.xbox.com/assets/a9/d0/a9d03015-2760-43df-8056-d23af1969289.gif?n=xbox_facebook_200x200.gif"
+      'https://compass-ssl.xbox.com/assets/a9/d0/a9d03015-2760-43df-8056-d23af1969289.gif?n=xbox_facebook_200x200.gif',
+    quantity: 0
   },
   {
     id: 4,
-    name: "XBOX",
+    name: 'XBOX',
     ean: 86786907698796,
     price: 12.5,
     imgUrl:
-      "https://compass-ssl.xbox.com/assets/a9/d0/a9d03015-2760-43df-8056-d23af1969289.gif?n=xbox_facebook_200x200.gif"
+      'https://compass-ssl.xbox.com/assets/a9/d0/a9d03015-2760-43df-8056-d23af1969289.gif?n=xbox_facebook_200x200.gif',
+    quantity: 0
   },
   {
     id: 5,
-    name: "XBOX",
+    name: 'XBOX',
     ean: 675674567567,
     price: 12.5,
     imgUrl:
-      "https://compass-ssl.xbox.com/assets/a9/d0/a9d03015-2760-43df-8056-d23af1969289.gif?n=xbox_facebook_200x200.gif"
+      'https://compass-ssl.xbox.com/assets/a9/d0/a9d03015-2760-43df-8056-d23af1969289.gif?n=xbox_facebook_200x200.gif',
+    quantity: 0
   },
   {
     id: 6,
-    name: "XBOX",
+    name: 'XBOX',
     ean: 456345737567,
     price: 12.5,
     imgUrl:
-      "https://compass-ssl.xbox.com/assets/a9/d0/a9d03015-2760-43df-8056-d23af1969289.gif?n=xbox_facebook_200x200.gif"
+      'https://compass-ssl.xbox.com/assets/a9/d0/a9d03015-2760-43df-8056-d23af1969289.gif?n=xbox_facebook_200x200.gif',
+    quantity: 0
   },
   {
     id: 7,
-    name: "XBOX",
+    name: 'XBOX',
     ean: 532462543656,
     price: 12.5,
     imgUrl:
-      "https://compass-ssl.xbox.com/assets/a9/d0/a9d03015-2760-43df-8056-d23af1969289.gif?n=xbox_facebook_200x200.gif"
+      'https://compass-ssl.xbox.com/assets/a9/d0/a9d03015-2760-43df-8056-d23af1969289.gif?n=xbox_facebook_200x200.gif',
+    quantity: 0
   },
   {
     id: 8,
-    name: "XBOX",
+    name: 'XBOX',
     ean: 34523546356426,
     price: 12.5,
     imgUrl:
-      "https://compass-ssl.xbox.com/assets/a9/d0/a9d03015-2760-43df-8056-d23af1969289.gif?n=xbox_facebook_200x200.gif"
+      'https://compass-ssl.xbox.com/assets/a9/d0/a9d03015-2760-43df-8056-d23af1969289.gif?n=xbox_facebook_200x200.gif',
+    quantity: 0
   }
 ];
--- src/app/main/shared/product.model.ts
+++ src/app/main/shared/product.model.ts
@@ -4,4 +4,5 @@ export class Product {
   ean: number;
   price: number;
   imgUrl: string;
+  quantity: number;
 }
