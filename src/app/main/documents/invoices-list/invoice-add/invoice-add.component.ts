d8a1bb998edc5ad6535a1d990dbca8d50ff0fa8f
hrodberaht <robpszczolka@gmail.com>
2018-09-11 09:59:55 +0200

    make invoice form

--- src/app/main/documents/invoices-list/invoice-add/invoice-add.component.ts
+++ src/app/main/documents/invoices-list/invoice-add/invoice-add.component.ts
@@ -1,27 +1,45 @@
-import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
-  selector: "app-invoice-add",
-  templateUrl: "./invoice-add.component.html",
-  styleUrls: ["./invoice-add.component.css"]
+  selector: 'app-invoice-add',
+  templateUrl: './invoice-add.component.html',
+  styleUrls: ['./invoice-add.component.css']
 })
 export class InvoiceAddComponent implements OnInit {
   invoiceForm: FormGroup;
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
-    this.invoiceForm = new FormGroup({
-      supplier: new FormControl(null, Validators.required),
-      invoiceNumber: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(3)
-      ])
+    this.invoiceForm = this.fb.group({
+      supplier: [],
+      invoiceNumber: [],
+      date: [],
+      products: this.fb.array([])
     });
+  }
+
+  get productsForms() {
+    return this.invoiceForm.get('products') as FormArray;
   }
 
   onSubmit() {
     console.log(this.invoiceForm);
+  }
+
+  addProduct() {
+    const product = this.fb.group({
+      ean: [],
+      name: [],
+      quantity: []
+    });
+    console.log(product);
+
+    this.productsForms.push(product);
+  }
+
+  deleteProduct(i) {
+    this.productsForms.removeAt(i);
   }
 }
